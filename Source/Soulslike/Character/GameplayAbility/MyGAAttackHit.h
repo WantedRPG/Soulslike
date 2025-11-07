7ad83b6a3514ec2ecb7f5cfdda323feb3d607cce
R-lth <bluearth0502@gmail.com>
2025-11-07 11:44:05 +0900

    :white_check_mark: 공격 판정 기능 임시 구현
    1. 공격 판정 타이밍
    - AnimNotify의 태그가 BPGA_AttackHit의 Trigger 태그로 설정돼 공격 판정 타이밍에 GA가 충돌 판정이 가능하도록 구현.
    
    2. GA
    - Ability(UMyGAAttackHit) → AbilityTask(UMyATAttackHit) → TargetActor(AMyTA_Target) 구조 완성
    - TargetActor에서 Sphere Trace로 피격 대상 탐지 후 Ability로 전달
    - Ability가 공격력(AttributeSet.AttackRate)을 SetByCaller(Data.Damage)로 전달하여 대상에 GE 적용
    - 디버그 표시 기능 추가 및 GameplayTag(Data.Damage) 정의

---
+++ Source/Soulslike/Character/GameplayAbility/MyGAAttackHit.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "MyGAAttackHit.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SOULSLIKE_API UMyGAAttackHit : public UGameplayAbility
+{
+	GENERATED_BODY()
+	
+public:
+	UMyGAAttackHit();
+
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+
+protected:
+	UFUNCTION()
+	void OnTraceResultCallback(const FGameplayAbilityTargetDataHandle& TargetDataHandle);
+
+	// TODO. GE 효과 지정
+	UPROPERTY(EditAnywhere, Category = "GAS")
+	TSubclassOf<class UGameplayEffect> AttackDamageEffect;
+
+	float CurrentLevel;
+};
---
+++ Source/Soulslike/Character/GameplayAbilityTargetActor/MyTA_Target.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbilityTargetActor.h"
+#include "MyTA_Target.generated.h"
+
+// 공격 대상 탐지
+UCLASS()
+class SOULSLIKE_API AMyTA_Target : public AGameplayAbilityTargetActor
+{
+	GENERATED_BODY()
+	
+public:
+	AMyTA_Target() = default;
+
+	virtual void StartTargeting(UGameplayAbility* Ability) override;
+	virtual void ConfirmTargetingAndContinue() override;
+	void SetShowDebug(bool InShowDebug) { bShowDebug = InShowDebug; }
+
+protected:
+	virtual FGameplayAbilityTargetDataHandle MakeTargetData() const;
+
+	bool bShowDebug = false;
+};
