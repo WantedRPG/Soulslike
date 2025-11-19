d7413104cec0f68f16d2ca155b88eeaf1f60aaf3
Jun0123 <76517458+Jun0123@users.noreply.github.com>
2025-11-19 18:57:26 +0900

    GA/ItemDataObejct 누락파일 추가

---
+++ Source/Soulslike/Item/GA/ItemDataObject.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "ItemDataObject.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SOULSLIKE_API UItemDataObject : public UObject
+{
+	GENERATED_BODY()
+	
+public:
+	FName ItemID;
+};
